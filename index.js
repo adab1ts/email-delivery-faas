7076e7a96eef28ba8b889d1db1296f2d29d87121
Carles Muiños <carles.ml.dev@gmail.com>
2018-01-30 21:00:33 +0100

    Add check function
    HTTP function to check whether the email server is ready to accept
    messages.

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
 'use strict';
 
+const nodemailer = require('nodemailer');
+const bunyan = require('bunyan');
+
 const config = require('./config.json');
 
+
+/**
+ *
+ */
 exports.ping = (request, response) => {
   // Everything is ok
   response.status(200).send('Pong!');
 };
+
+/**
+ *
+ */
+function getTransporter() {
+  const logger = bunyan.createLogger({ name: 'nodemailer' });
+  logger.level('trace');
+
+  return nodemailer.createTransport({
+    service: config.transport.service,
+    auth: config.transport.auth,
+    logger,
+    debug: true
+  });
+}
+
+/**
+ *
+ */
+exports.check = (request, response) => {
+  const transporter = getTransporter();
+
+  transporter.verify((error, success) => {
+    if (error) {
+      console.log(`Error: ${error}`);
+      response.status(400).send('Error: Server is not ready to accept messages');
+    } else {
+      response.status(200).send('Success: Server ready to take our messages');
+    }
+  });
+}
